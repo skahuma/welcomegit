8d319d17666a58d6c26ecee93fe5fa5887982d03
skahuma <skahuma@github.com>
2017-01-09 16:48:31 +0300

    first commit

---
+++ solomon.py
@@ -0,0 +1,20 @@
+
+total = 15000
+sam = 35/100 * total
+solomon = 65/100 * total
+#print ("sam gets %d and solomon gets %d"% (sam,solomon))
+
+x=0
+while x<51:
+	#print (x)
+	x=x+1
+
+
+class_names=["solomon","samson","alban","ivan"]
+print (class_names)
+print (class_names[3])
+
+
+for item in class_names:
+	print (item)
+	
